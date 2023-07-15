4cf83b992d37575bec02475a4c3df3ade2604ba3
PKishoreraj <113040359+PKishoreraj@users.noreply.github.com>
2023-07-15 22:22:44 +0530

    Add files via upload

---
+++ Python Project1.py
@@ -0,0 +1,13 @@
+from tkinter import* 
+root= Tk()
+
+root.title("Driver Lincence")
+root.geometry("400x200")
+
+label_id=Label(root)
+label_name=Label(root)
+label_dob=Label(root)
+label_pin=Label(root)
+label_address=Label(root)
+label_vehicle_type=Label(root)
+
